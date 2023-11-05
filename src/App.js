08b8071efbe8daf95af60bed1b239bdec2045316
Petr Bubela <petrbe@moravia.com>
2023-11-05 22:40:13 +0100

    Add input fields and buttons to App component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 function App() {
   const value = 'World 2';
-  return <div>Hello {value}</div>;
+  
+  return (
+    <div>
+      <div>Hello {value}</div>
+      <input type="text" placeholder="Enter URL" />&nbsp;
+      <br></br>
+      <button>Analyze</button>&nbsp;
+      <button>Generate</button>
+    </div>
+  );
 }
 
 export default App;
